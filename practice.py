8571b3d1bb1c6afd6bb9da5955e79291553b736a
Arun KC <aroon.kc2003@gmail.com>
2026-04-13 10:27:58 +0545

    untracking all unnecessary file

--- practice.py
+++
@@ -1,5 +0,0 @@
-countries=['Nepal','Iran','Poland','Finland','Nederland','Japan','China']
-sub_string="land"
-matching_lst=[x for x in countries if sub_string in x]
-print("countries are:",countries)
-print("countries containig substring land:",matching_lst)
